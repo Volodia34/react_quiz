928f41c684db50264b43a265b48346d7ab69194a
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2024-01-24 15:34:38 +0200

    State-management

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useContext, useReducer, useState} from 'react';
 import Question from "./Question";
 
+import {QuizContext} from "../contexts/quiz";
+
+
 const Quiz = () => {
+    const [quizState,dispatch] = useContext(QuizContext)
+    console.log(quizState)
+    console.log('render', quizState)
+
     return (
         <div className='quiz'>
             <div className='score'>Quiz</div>
-            <Question/>
-            <div className="next-button">Next</div>
+            <Question question={quizState.question}/>
+            <div className="next-button" onClick={() =>  dispatch({type: 'NEXT_QUESTION'})}>Next Question {quizState.currentQuestionIndex}</div>
         </div>
 
     );
